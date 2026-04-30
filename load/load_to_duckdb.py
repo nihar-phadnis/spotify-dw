f248f6a7507915d1cf11f7110d102d96d2f9a0a2
Nihar Phadnis <nihar.sg8@gmail.com>
2026-04-30 23:51:57 +0100

    updated to include optional sys args to navigate db to dev or prod

--- load/load_to_duckdb.py
+++ load/load_to_duckdb.py
@@ -2,21 +2,19 @@ import duckdb
 from pathlib import Path
 import sys
 
-# load/load_to_duckdb.py
 base_path = Path(__file__).parent.parent / "data/raw"
-db_path = Path(__file__).parent.parent / "spotify.duckdb"
 
-def load_to_duckdb(endpoint_type: str): 
-    
+def load_to_duckdb(endpoint_type: str, db_path: Path): 
     folder = base_path / endpoint_type
-
     with duckdb.connect(str(db_path)) as conn:
         conn.execute("CREATE SCHEMA IF NOT EXISTS raw")
- 
         conn.execute(f"""
             CREATE OR REPLACE TABLE raw.{endpoint_type} AS
             SELECT * FROM read_json_auto('{folder}/**/*.json')
         """)
             
 if __name__ == "__main__":
-    load_to_duckdb(sys.argv[1])
+    endpoint = sys.argv[1]
+    db_name = sys.argv[2] if len(sys.argv) > 2 else "dev"
+    db_path = Path(__file__).parent.parent / f"{db_name}.duckdb"
+    load_to_duckdb(endpoint, db_path)
