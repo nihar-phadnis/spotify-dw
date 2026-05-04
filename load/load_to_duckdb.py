81544453ea9aad5f821ce8f2e8ac73fdc4df51e6
Nihar Phadnis <nihar.sg8@gmail.com>
2026-05-04 12:38:00 +0100

    add staging schema tests and fix load logic for latest file per endpoint

--- load/load_to_duckdb.py
+++ load/load_to_duckdb.py
@@ -1,16 +1,30 @@
 import duckdb
 from pathlib import Path
-import sys
+import sys, os
 
-base_path = Path(__file__).parent.parent / "data/raw"
+#Valid Endpoints for load to duckdb stage. 
+VALID_ENDPOINTS = ["recently_played", "top_tracks", "playlist_tracks"]
+
+base_path = (Path(__file__).parent.parent / "data/raw")
 
 def load_to_duckdb(endpoint_type: str, db_path: Path): 
+    
+    if endpoint_type not in VALID_ENDPOINTS: 
+        raise ValueError(f"Invalid endpoint: {endpoint_type}")
+    
     folder = base_path / endpoint_type
+
+    if endpoint_type == "recently_played": 
+        file_path = f"{folder}/**/*.json"
+    else: 
+        latest = max((folder).rglob("*.json"), key = os.path.getctime)
+        file_path = str(latest)
+    
     with duckdb.connect(str(db_path)) as conn:
         conn.execute("CREATE SCHEMA IF NOT EXISTS raw")
         conn.execute(f"""
             CREATE OR REPLACE TABLE raw.{endpoint_type} AS
-            SELECT * FROM read_json_auto('{folder}/**/*.json')
+            SELECT * FROM read_json_auto('{file_path}')
         """)
             
 if __name__ == "__main__":
